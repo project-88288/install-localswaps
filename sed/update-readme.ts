84dfbffa6cdbd18828e884200ce7c6230b5ba56b
chaiya srisawat <chaiya.88288@gmail.com>
2023-08-09 13:15:15 +0700

    add @opz

---
+++ sed/update-readme.ts
@@ -0,0 +1,54 @@
+
+import { Env, task } from "@terra-money/terrain";
+
+export function update_readme(){
+    
+    task(async () => {
+        process.env.
+    });
+
+    const factory_addr_mainnet = ""
+    const factory_addr_testnet = ""
+    const factory_addr_localterra = ""
+
+    const pair_codeID_mainnet = 0
+    const pair_codeID_testnet = 0
+    const pair_codeID_localterra = 0
+
+    const token_codeID_mainnet = 0
+    const token_codeID_testnet = 0
+    const token_codeID_localterra =0
+
+    const router_addr_mainnet = ""
+    const router_addr_testnet = ""
+    const router_addr_localterra = ""
+
+}
+
+/*
+
+* terraswap_factory
+
+   Mainnet: `terra1466nf3zuxpya8q9emxukd7vftaf6h4psr0a07srl5zw74zh84yjqxl5qul`
+
+   Testnet: `terra1jha5avc92uerwp9qzx3flvwnyxs3zax2rrm6jkcedy2qvzwd2k7qk7yxcl`
+
+* terraswap_pair
+
+   Mainnet (CodeID): 5
+
+   Testnet (CodeID): 84
+
+* terraswap_token
+
+   Mainnet (CodeID): 4
+
+   Testnet (CodeID): 83
+
+* terraswap_router
+
+   Mainnet: `terra13ehuhysn5mqjeaheeuew2gjs785f6k7jm8vfsqg3jhtpkwppcmzqcu7chk`
+
+   Testnet: `terra1xp6xe6uwqrspumrkazdg90876ns4h78yw03vfxghhcy03yexcrcsdaqvc8`
+
+   */
